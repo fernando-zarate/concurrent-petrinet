7091d75c938466a613c0f3d63134f8bd5773b548
Ignacio Giraudo <ignacio.jose.giraudo@mi.unc.edu.ar>
2026-06-29 13:35:50 -0300

    add informe

--- src/Main.java
+++ src/Main.java
@@ -23,8 +23,8 @@ public class Main {
     public static void main(String[] args) {
         Logger logger = new Logger();
         PetriNet petriNet = new PetriNet(MAX_INVARIANTS, logger);
-        //PolicyInterface policy = new PrioritizedPolicy();
-        PolicyInterface policy = new RandomPolicy();
+         PolicyInterface policy = new PrioritizedPolicy();
+        // PolicyInterface policy = new RandomPolicy();
         MonitorInterface monitor = new Monitor(petriNet, policy);
 
         // Create the segments based on the SEGMENTS_SETUP configuration and the transitions of the petri net.
@@ -32,10 +32,10 @@ public class Main {
         for (int i = 0; i < SEGMENTS_SETUP.length; i++) {
             for (int j = 0; j < SEGMENTS_SETUP[i][0][0]; j++) {
                 segments.add(new Segment(SEGMENTS_SETUP[i][1], monitor));
-                //System.out.printf("THREAD-Main: Created segment %d for transitions %s.\n", segments.size() - 1, Arrays.toString(SEGMENTS_SETUP[i][1]));
+                // System.out.printf("THREAD-Main: Created segment %d for transitions %s.\n", segments.size() - 1, Arrays.toString(SEGMENTS_SETUP[i][1]));
             }
         }
-        //System.out.printf("THREAD-Main: Created %d segments.\n", segments.size());
+        // System.out.printf("THREAD-Main: Created %d segments.\n", segments.size());
 
         long startTime = System.currentTimeMillis();
 
@@ -46,9 +46,9 @@ public class Main {
             Thread thread = new Thread(segment);
             threads.add(thread);
             thread.start();
-            //System.out.printf("THREAD-Main: Starting thread %d/%d.\n", threads.size() - 1, segments.size() - 1);
+            // System.out.printf("THREAD-Main: Starting thread %d/%d.\n", threads.size() - 1, segments.size() - 1);
         }
-        //System.out.printf("THREAD-Main: All %d/%d threads have been started.\n", threads.size(), segments.size());
+        // System.out.printf("THREAD-Main: All %d/%d threads have been started.\n", threads.size(), segments.size());
 
         // Wait for the last transition to complete MAX_INVARIANTS.
         int lastTransitionCounterIndex = petriNet.getTransitionCounters().length - 1;
@@ -60,16 +60,16 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            //System.out.printf("THREAD-Main: Waiting for the network to be drained... (T%d: %d/%d)\n", lastTransitionCounterIndex, lastTransitionCounter, MAX_INVARIANTS);
+            // System.out.printf("THREAD-Main: Waiting for the network to be drained... (T%d: %d/%d)\n", lastTransitionCounterIndex, lastTransitionCounter, MAX_INVARIANTS);
         }
-        //System.out.printf("THREAD-Main: Network drained. Interrupting threads...\n");
+        // System.out.printf("THREAD-Main: Network drained. Interrupting threads...\n");
 
         // Interrupt all threads.
         for (Thread thread : threads) {
             thread.interrupt();
             //System.out.printf("THREAD-Main: Interrupting thread %s.\n", thread.getName());
         }
-        //System.out.printf("THREAD-Main: All threads have been interrupted. Waiting for them to finish...\n");
+        // System.out.printf("THREAD-Main: All threads have been interrupted. Waiting for them to finish...\n");
 
         // Wait for all threads to finish.
         for (Thread thread : threads) {
@@ -79,7 +79,7 @@ public class Main {
                 e.printStackTrace();
             }
         }
-        //System.out.printf("THREAD-Main: All threads have finished.\n");
+        // System.out.printf("THREAD-Main: All threads have finished.\n");
         
         // Print the final counters for the transitions of interest.
         int[] counters   = petriNet.getTransitionCounters();
@@ -87,7 +87,7 @@ public class Main {
         int highRisk     = counters[4]; // T4
         int bankTransfer = counters[6]; // T6
 
-        //int t4Eligible = ((Monitor) monitor).getT4EligibleCount();
+        // int t4Eligible = ((Monitor) monitor).getT4EligibleCount();
 
         System.out.printf("Credit/Debit:    %d%n", creditCard);
         System.out.printf("High-Risk:       %d%n", highRisk);
